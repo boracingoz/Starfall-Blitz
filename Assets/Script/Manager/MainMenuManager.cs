5ce1b3c3ae701f2285c3c4fb89bd81f3c8f08208
boracingoz26 <boracingoz@gmail.com>
2025-06-12 02:20:52 +0300

    Implemented main menu logic: show MenuPanel on start, toggle between MenuPanel and SettingsPanel, and persist audio/vibration settings using PlayerPrefs

---
+++ Assets/Script/Manager/MainMenuManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject menuPanel;
+    public GameObject settingsPanel;
+
+    [Header("Audio Settings")]
+    public Slider volumeSlider;
+    public Toggle vibrationToggle;
+
+    private const string LastLevelKey = "LastLevel";
+    private const string VolumeKey = "Volume";
+    private const string VibrationKey = "Vibration";
+
+    private void Start()
+    {
+        menuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+
+        volumeSlider.value = PlayerPrefs.GetFloat(VolumeKey, 1f); 
+        vibrationToggle.isOn = PlayerPrefs.GetInt(VibrationKey, 1) == 1;
+
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    public void OnPlayButtonPressed()
+    {
+        string levelToLoad = PlayerPrefs.HasKey(LastLevelKey) ? PlayerPrefs.GetString(LastLevelKey) : "Level1";
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void OnSettingsButtonPressed()
+    {
+        menuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OnBackFromSettings()
+    {
+        settingsPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+
+    public void OnVolumeChanged(float value)
+    {
+        AudioListener.volume = value;
+        PlayerPrefs.SetFloat(VolumeKey, value);
+    }
+
+    public void OnVibrationToggled(bool isOn)
+    {
+        PlayerPrefs.SetInt(VibrationKey, isOn ? 1 : 0);
+    }
+}
